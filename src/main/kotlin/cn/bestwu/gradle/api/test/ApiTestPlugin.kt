00e9f3d533b698d9e7ebeeb3b14507d06ae8da5f
Peter Wu <piterwu@outlook.com>
2018-02-09 22:54:02 +0800

    初步实现

---
+++ src/main/kotlin/cn/bestwu/gradle/api/test/ApiTestPlugin.kt
@@ -0,0 +1,67 @@
+package cn.bestwu.gradle.api.test
+
+import cn.bestwu.apidoc.ApidocExtension
+import cn.bestwu.gradle.apidoc.ApidocPlugin
+import cn.bestwu.gradle.profile.ProfileExtension
+import cn.bestwu.gradle.profile.ProfilePlugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.jvm.tasks.Jar
+
+/**
+ *
+ * 注册task
+ *
+ * @author Peter Wu
+ */
+class ApiTestPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        project.plugins.apply(ApidocPlugin::class.java)
+        project.plugins.apply(ProfilePlugin::class.java)
+
+        project.afterEvaluate {
+            val version = it.findProperty("api.test.version") ?: "1.3.8-SNAPSHOT"
+            val starterDocVersion = it.findProperty("api.starter-doc.version") ?: "0.0.1-SNAPSHOT"
+            project.dependencies.add("compile", "cn.bestwu:api-test:$version")
+            project.dependencies.add("compileOnly", "cn.bestwu:starter-apidoc:$starterDocVersion")
+            project.dependencies.add("testCompile", "cn.bestwu:starter-apidoc:$starterDocVersion")
+        }
+
+        val paths = ((project.findProperty("api.test.paths")
+                ?: "_t") as String).split(",").filter { it.isNotBlank() }.map { it.trim() }
+
+        project.tasks.getByName("clean") {
+            it.doFirst {
+                paths.forEach {
+                    project.file("src/main/resources/$it/md").deleteRecursively()
+                    project.file("src/main/resources/$it/html").deleteRecursively()
+                }
+            }
+        }
+
+        project.extensions.configure(ApidocExtension::class.java) {
+            val applicationHost = project.findProperty("generator.application.host") as? String
+            if (!applicationHost.isNullOrBlank())
+                it.defaultHost = applicationHost!!
+            val applicationName = project.findProperty("application.name") as? String
+            if (!applicationName.isNullOrBlank())
+                it.projectName = applicationName!!
+        }
+
+        val releases = ((project.findProperty("api.test.releases")
+                ?: "release") as String).split(",").filter { it.isNotBlank() }.map { it.trim() }
+        project.extensions.configure(ProfileExtension::class.java) {
+            it.closure {
+                if (releases.contains(it.active)) {
+                    val jar = project.tasks.getByName("jar") as Jar
+                    jar.exclude(paths)
+                    project.configurations.getByName("compile").dependencies.removeIf {
+                        it.group == "cn.bestwu" && it.name == "api-test"
+                    }
+                }
+            }
+        }
+    }
+
+}
