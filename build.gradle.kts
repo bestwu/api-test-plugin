68d83421a5c312d7d5f354bc99374f09b91d1857
Peter Wu <piterwu@outlook.com>
2018-03-21 14:32:48 +0800

    0.0.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,18 @@
 import org.gradle.api.JavaVersion
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.kotlin.dsl.*
+import org.jetbrains.dokka.gradle.DokkaTask
 
 val kotlinVersion = "1.2.30"
 
 plugins {
     idea
     kotlin("jvm") version "1.2.30"
-    id("cn.bestwu.plugin-publish") version "0.0.18"
+    id("cn.bestwu.plugin-publish") version "0.0.19"
 }
 
-group = "gradle.plugin.cn.bestwu.gradle"
+group = "cn.bestwu.gradle"
 version = "0.0.13"
-
-tasks.withType(JavaCompile::class.java) {
-    options.encoding = "UTF-8"
-}
 
 repositories {
     mavenLocal()
@@ -26,7 +23,7 @@ repositories {
 dependencies {
     compile(gradleApi())
     compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    compile("gradle.plugin.cn.bestwu.gradle:profile-plugin:1.4.14")
+    compile("gradle.plugin.cn.bestwu.gradle:profile-plugin:1.4.16")
     compile("gradle.plugin.cn.bestwu.gradle:apidoc-plugin:1.2.9")
 
     testCompile("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
@@ -39,5 +36,11 @@ idea {
         isDownloadSources = true
         outputDir = java.sourceSets["main"].java.outputDir
         testOutputDir = java.sourceSets["test"].java.outputDir
+    }
+}
+
+tasks {
+    "dokkaJavadoc"(DokkaTask::class) {
+        noStdlibLink = true
     }
 }
