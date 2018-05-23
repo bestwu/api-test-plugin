c32377291c8b3f1c2753303e08c50b34a99bba25
Peter Wu <piterwu@outlook.com>
2018-05-23 15:54:20 +0800

    release 0.0.47

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "cn.bestwu.gradle"
-version = "0.0.46"
+version = "0.0.47"
 
 repositories {
     mavenLocal()
@@ -24,7 +24,7 @@ dependencies {
     compile(gradleApi())
     compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     compile("gradle.plugin.cn.bestwu.gradle:profile-plugin:1.4.19")
-    compile("gradle.plugin.cn.bestwu.gradle:apidoc-plugin:1.2.29")
+    compile("gradle.plugin.cn.bestwu.gradle:apidoc-plugin:1.2.30")
 
     testCompile("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
